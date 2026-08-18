6dd957d66319fa1965d8ebc87e7c4fefe8abe698
Elifsu Tatli <elifsuttatli@gmail.com>
2026-08-18 10:53:21 +0300

    Update

--- keypad_test.py
+++ keypad_test.py
@@ -2,12 +2,8 @@ from gpiozero import DigitalOutputDevice, DigitalInputDevice
 from time import sleep
 
 
-# Keypad'in ilk 4 hattı
 ROWS = [4, 7, 8, 9]
-
-# Son 4 hattı
 COLS = [10, 11, 18, 19]
-
 
 KEYS = [
     ["1", "2", "3", "A"],
@@ -29,45 +25,42 @@ cols = [
 
 
 print("KEYPAD TEST BASLADI")
-print("Tuslara tek tek basin...")
+print("Tuslara basin...")
 
 
 try:
-
     while True:
 
         for row_index, row in enumerate(rows):
 
-            # Sadece bu satiri LOW yap
+            # O an taranan satırı LOW yap
             row.off()
-
             sleep(0.002)
 
             for col_index, col in enumerate(cols):
 
-                if not col.value:
+                # Tuşa BASILDIYSA
+                if col.is_active:
 
                     key = KEYS[row_index][col_index]
 
                     print("BASILAN TUS:", key)
 
-                    # Tus birakilana kadar bekle
-                    while not col.value:
+                    # Tuş bırakılana kadar bekle
+                    while col.is_active:
                         sleep(0.02)
 
                     sleep(0.05)
 
-            # Satiri tekrar HIGH yap
+            # Satırı tekrar HIGH yap
             row.on()
 
 
 except KeyboardInterrupt:
-
     print("\nTest kapatildi.")
 
 
 finally:
-
     for row in rows:
         row.close()
 
